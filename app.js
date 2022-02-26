aa7926dd7374b686e9ac0bc346eed5b09458029c
rearwindowsoda <mat.owc@o2.pl>
2022-02-26 02:49:53 +0100

    trying to get https working

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const https = require('https');
-const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const credentials = {key: fs.readFileSync(path.join(__dirname, 'cert', 'cert.key')),cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))}
 const app = express();
 
 //middleware
-app.enable('trust proxy');
-
+app.all('*', ensureSecure)
+const sslServer = https.createServer(credentials, app);
+sslServer.listen(443)
 app.use(express.static(path.join(__dirname + '/public')));
 
-
-//routes
-
-
-//create and  start server
-// const httpServer = http.createServer(app);
-const sslServer = https.createServer(credentials, app);
-sslServer.listen(3443)
-// httpServer.listen(3000)
+function ensureSecure(req, res, next){
+    if(req.secure){
+        // OK, continue
+        return next();
+    };
+    res.redirect('https://' + req.hostname + req.url);
+}
 
