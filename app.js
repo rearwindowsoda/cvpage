bdba3b82fe1c8da92337272deacc1850ae73c770
rearwindowsoda <mat.owc@o2.pl>
2022-02-26 03:07:46 +0100

    trying to get https working

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const https = require('https');
 const path = require('path');
 const fs = require('fs');
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
 const credentials = {key: fs.readFileSync(path.join(__dirname, 'cert', 'cert.key')),cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))}
 const app = express();
 
 //middleware
-app.use(redirectToHTTPS());
+app.all('*', function(req, res, next){
+    if (req.secure) {
+        return next();
+    }
+
+    res.redirect('https://'+req.hostname + ':' + app.get('secPort') + req.url);
+});
 const sslServer = https.createServer(credentials, app);
 sslServer.listen(443)
 app.use(express.static(path.join(__dirname + '/public')));
