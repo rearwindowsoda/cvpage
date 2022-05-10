89c05177ac223964134415a9eccaa3dbbc1e749b
MateuszOwczarek <mat.owc@o2.pl>
2022-05-10 21:30:56 +0200

    Bumped app version, changed app start (from polyglot to regular express.js) in app.js, clean unused imports

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const https = require('https');
-const http = require('http');
 const path = require('path');
-const httpolyglot = require('httpolyglot');
-const fs = require('fs');
-const credentials = {key: fs.readFileSync(path.join(__dirname, 'cert', 'cert.key')),cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))}
 const app = express();
 
 //middleware
 app.use(express.static(path.join(__dirname + '/public')));
 
-httpolyglot.createServer(credentials, app).listen(443);
-
-
-
-
+app.listen(3000, () => {
+	console.log('Server started on port 3000. \n ', 'http://localhost:3000');
+});
