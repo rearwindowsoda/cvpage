fa44e128c666510b5cf1fac04e959b3948d1bb38
MateuszOwczarek <mat.owc@o2.pl>
2022-06-04 00:43:16 +0200

    Added new entry to courses/education summary | Replaced portfolio app image with its newer version screenshot

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 //middleware
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.listen(3000, () => {
-	console.log('Server started on port 3000. \n ', 'http://localhost:3000');
+  console.log("Server started on port 3000. \n ", "http://localhost:3000");
 });
